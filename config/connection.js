4d4c515d120320b4e845458926a0ad21f7efe2f6
Manny Barboza <mannyb1077@gmail.com>
2019-06-27 21:06:53 -0600

    jaws"

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 //Setup mysql connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "B00tcamp",
-    database: "burger_db"
-});
+var 
 
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 }else{
-
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "B00tcamp",
+        database: "burger_db"
+    });
 };
 
 connection.connect(function(err){
